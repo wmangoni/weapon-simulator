75c6781bcc9e87decf09eda034da97154d6efbca
wmangoni <willrpg@hotmail.com>
2021-02-25 10:08:55 -0300

    Ajuste em extensão de arquivo e sintaxe do print para versão do python

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ bullets = [
 
 myPistol = weapon.P90(ammo.Ammo_9x18(), 1.1, magazine.Magazine_9x18_15(bullets), None)
 
-print 'Nome: ' + myPistol.name + "\n" \
+print('Nome: ' + myPistol.name + "\n" \
       + 'Tipo: ' + str(myPistol.type) + "\n" \
       + 'Peso: ' + str(myPistol.weight) + ' kg' + "\n" \
       + 'Acessórios: ' + str(myPistol.attachments) + "\n" \
       + 'Munição: ' + myPistol.ammo.name + "\n" \
-      + 'Carregador: ' + myPistol.magazine.name + "\n"
+      + 'Carregador: ' + myPistol.magazine.name + "\n")
 
-print 'chackMagazine - ' + str(myPistol.checkMagazine())
+print('chackMagazine - ' + str(myPistol.checkMagazine()))
 
 myPistol.shot()
-print 'shot'
+print('shot')
 
-print 'chackMagazine - ' + str(myPistol.checkMagazine())
+print('chackMagazine - ' + str(myPistol.checkMagazine()))
